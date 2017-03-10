d912472a60efc2ad4f6dded3312d6d0a26cdca0c
test <1060656096@qq.com>
2017-03-10 15:10:50 +0800

    aaa: 增加单元测试框架

---
+++ tests/TestInit.php
@@ -0,0 +1,10 @@
+<?php
+namespace Wei\Cards\Tests;
+
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+}
+else {
+    throw new \Exception('Can\'t find autoload.php. Did you install dependencies via composer?');
+}
---
+++ tests/WeiTestCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace Wei\Cards\Tests;
+
+/**
+ * 单元测测试基类
+ * Class WeiTestCase
+ * @package Wei\Cards\Tests
+ */
+abstract class WeiTestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
