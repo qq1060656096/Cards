9d38b5d3aae3efa9ab26132983a153043560b406
test <1060656096@qq.com>
2017-03-10 18:11:46 +0800

    caa: 增加测试男性18位身份证

--- tests/IDCards/IDCard18Test.php
+++ tests/IDCards/IDCard18Test.php
@@ -2,7 +2,9 @@
 namespace Wei\Cards\Tests\IDCards;
 
 
+use Wei\Cards\IDCards\IDCard;
 use Wei\Cards\IDCards\IDCard18;
+use Wei\Cards\IDCards\IDCardBase;
 use Wei\Cards\Tests\WeiTestCase;
 
 /**
@@ -23,5 +25,21 @@ class IDCard18Test extends WeiTestCase {
         $this->assertEquals("18", $idCard->getLen());
         $this->assertEquals("18", $idCard->getType());
         $this->assertEquals("5121", $idCard->getSequenceCode());
+    }
+
+    /**
+     * 测试男性身份证
+     */
+    public function testMale(){
+        $idCard = IDCard18::getInstance('511521198911079996');
+        $this->assertTrue($idCard ? true : false);
+        $this->assertEquals("51", $idCard->getProvince());
+        $this->assertEquals("15", $idCard->getCity());
+        $this->assertEquals("21", $idCard->getArea());
+        $this->assertEquals("1989-11-07", $idCard->getBirthday());
+        $this->assertEquals(IDCardBase::SEX_MALE, $idCard->getSex());
+        $this->assertEquals("18", $idCard->getLen());
+        $this->assertEquals("18", $idCard->getType());
+        $this->assertEquals("9996", $idCard->getSequenceCode());
     }
 }
